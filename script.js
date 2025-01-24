f9bc91dad973b60250796f8cf7bd5c150560a2c2
Yousaf Summiya <summiya.yousaf@leerling.gent.be>
2025-01-24 13:33:20 +0100

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 //functie om data op te halen
 const apiURL = "https://data.stad.gent/api/explore/v2.1/catalog/datasets/bezetting-parkeergarages-real-time/records?limit=20";
+//voer de leeflet map toe
+let map = L.map('map').setView([51.505, 3.73], 13);
+
+//voeg de basemap toe
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 async function fetchAndDisplayParkings() {
     try {
@@ -18,18 +26,20 @@ async function fetchAndDisplayParkings() {
             const occupied = totalcapacity - availablecapacity;
             const status = isopennow ? "open" : "gesloten";
             const parkingDiv = document.createElement("div");
-parkingDiv.classList.add("parking");
+            parkingDiv.classList.add("parking");
 
-parkingDiv.innerHTML = `<h3>${name}</h3>
+            parkingDiv.innerHTML = `<h3>${name}</h3>
             <p><strong>max: </strong>${totalcapacity}</p>
             <p><strong>Bezet: </strong>${occupation}</p>
             <p><strong>Beschikbaar:</strong> ${availablecapacity}</p>
-            <p class ="${isopennow ? 'open':'gesloten'}"> <strong>${status}</strong></p>`;
+            <p class ="${isopennow ? 'open' : 'gesloten'}"> <strong>${status}</strong></p>`;
 
-parkingContainer.appendChild(parkingDiv);
+            parkingContainer.appendChild(parkingDiv);
 
 
         });
+
+        DisplayParkingsOnMap(location);
     } catch (error) {
         console.error("Fout bij data fetch:", error);
     } finally {
